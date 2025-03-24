b6da7c171df25b26454d908c93824049a86cd5d1
Chedva <r0556730270@gmail.com>
2025-03-24 14:24:22 +0200

    c2

---
+++ TodoApi/ToDoDbContextt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace TodoApi;
+
+public partial class ToDoDbContextt : DbContext
+{
+    public ToDoDbContextt()
+    {
+    }
+
+    public ToDoDbContextt(DbContextOptions<ToDoDbContextt> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Item> Items { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("name=ToDoDB", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.22-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8_general_ci")
+            .HasCharSet("utf8");
+
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("User");
+
+            entity.Property(e => e.Name).HasMaxLength(30);
+            entity.Property(e => e.password)
+                .HasMaxLength(20)
+                .HasColumnName("password");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
